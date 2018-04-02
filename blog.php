905d9ec34bf0c2c01d307f45f8ea45194fe2eb33
Harsh Kashyap <hrs307@gmail.com>
2018-04-02 19:18:41 +0530

    add media queries to events section

--- blog.php
+++ blog.php
@@ -81,7 +81,7 @@
 }
 
 ?>
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
   <script src="js/blog.js"></script>
   
 </body>
