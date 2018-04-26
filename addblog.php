12328b8434d53364303420382e13320c0055926b
ashish aggarwal <ashish09101998@gmail.com>
2018-04-26 16:32:46 +0530

    editor added

--- addblog.php
+++ addblog.php
@@ -12,7 +12,6 @@ if(isset($_SESSION)){
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -49,7 +48,7 @@ if(isset($_SESSION)){
     exit();
 }
 ?>
-    <form action="addprocess.php" method="post">
+  <form action="addprocess.php" method="post">
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="title">Title</label>
@@ -66,7 +65,7 @@ if(isset($_SESSION)){
   </div>
   <div class="form-group">
     <label for="content">Content</label>
-    <textarea name="content" cols="30" rows="10" class="form-control" required></textarea>
+    <textarea name="content" cols="30" rows="10" class="form-control" id="myeditor" required></textarea>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
@@ -82,6 +81,10 @@ if(isset($_SESSION)){
 </form>
     </div>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="ckeditor/ckeditor.js"></script>
+    <script type="text/javascript">
+	CKEDITOR.replace('myeditor');
+    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
